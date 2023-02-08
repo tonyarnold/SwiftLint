e9fe568960d16ad45381b28356c56b82b5b087dc
Tony Arnold <tony@thecocoabots.com>
2023-02-08 11:26:35 +1100

    Move to being a prebuild command

--- Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
+++ Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
@@ -18,8 +18,7 @@ struct SwiftLintPlugin: BuildToolPlugin {
     private func createBuildCommands(inputFiles: [Path],
                                      packageDirectory: Path,
                                      workingDirectory: Path,
-                                     tool: PluginContext.Tool,
-                                     preventInputFileConsumption: Bool = false
+                                     tool: PluginContext.Tool
     ) throws -> [Command] {
         if inputFiles.isEmpty {
             // Don't lint anything if there are no Swift source files in this target
@@ -39,42 +38,14 @@ struct SwiftLintPlugin: BuildToolPlugin {
         }
         arguments += inputFiles.map(\.string)
 
-        let buildCommandInputFiles: [Path]
-        if preventInputFileConsumption {
-            // This is a workaround for an issue that affects Xcode targets under Xcode 14.x (FB11835329, FB11877146):
-            // If a build tool plugin is applied to an Xcode target and lists `.swift` source files as inputs of
-            // the build commands it generates, then those `.swift` source files aren't seen by Xcode's build
-            // system (as they've been "consumed" by the plugin command).
-            //
-            // The workaround creates a new symbolic link to the project directory within the build plugin's working 
-            // directory, then references every input file via that symbolically linked directory as the input files
-            // for the build command. This prevents Xcode "consuming" the input files, and allows further processing 
-            // such as compilation.
-            let xcodeProjDirSymlink = workingDirectory.appending("ProjectDir")
-            try? FileManager.default.removeItem(atPath: xcodeProjDirSymlink.string)
-            try FileManager.default.createSymbolicLink(
-                atPath: xcodeProjDirSymlink.string,
-                withDestinationPath: packageDirectory.string
-            )
-
-            buildCommandInputFiles = inputFiles.map { inputFile in
-                let relativePath = inputFile.relative(to: packageDirectory)
-                return xcodeProjDirSymlink.appending(subpath: relativePath.string)
-            }
-        } else {
-            buildCommandInputFiles = inputFiles
-        }
-
         let nonExistentOutputDirectory = workingDirectory.appending(UUID().uuidString)
-        try FileManager.default.createDirectory(atPath: nonExistentOutputDirectory.string, withIntermediateDirectories: false)
 
         return [
-            .buildCommand(
+            .prebuildCommand(
                 displayName: "SwiftLint",
                 executable: tool.path,
                 arguments: arguments,
-                inputFiles: buildCommandInputFiles,
-                outputFiles: [nonExistentOutputDirectory]
+                outputFilesDirectory: nonExistentOutputDirectory
             )
         ]
     }
@@ -93,8 +64,7 @@ extension SwiftLintPlugin: XcodeBuildToolPlugin {
             inputFiles: inputFilePaths,
             packageDirectory: context.xcodeProject.directory,
             workingDirectory: context.pluginWorkDirectory,
-            tool: try context.tool(named: "swiftlint"),
-            preventInputFileConsumption: true
+            tool: try context.tool(named: "swiftlint")
         )
     }
 }
